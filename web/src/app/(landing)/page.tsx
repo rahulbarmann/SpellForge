20193ee125625c7cc271c965fcad360910d116d3
VanshSahay <vansh.sahay1234@gmail.com>
2024-08-26 01:50:39 +0530

    add basic landing page

--- web/src/app/(landing)/page.tsx
+++ web/src/app/(landing)/page.tsx
@@ -15,7 +15,8 @@ import RPC from "../../api/viemRPC";
 // import RPC from "./web3RPC";
 
 // ADD IN DOTENV
-const clientId = process.env.WEB3AUTH_CLIENT_ID as string; // get from https://dashboard.web3auth.io
+const clientId =
+    "BIn_YLNB6DaqsmCw5gTK9VrdOKeDSUixUnOqVFdcr_HCC_sUIDL3ZlID3RAHqSm3tXTUr5LcHi5-_IjqiwmCqqc"; // get from https://dashboard.web3auth.io
 
 const chainConfig = {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
@@ -94,10 +95,28 @@ function App() {
     );
 
     return (
-        <div className="container">
-            <div className="grid">
+        <div className="flex flex-col h-full justify-between items-center">
+            {/* <div className="grid">
                 {loggedIn ? loggedInView : unloggedInView}
+            </div> */}
+            <div className="flex flex-col items-center justify-center h-full">
+                <div className="m-8 p-3 border-transparent rounded-full transition border-4 hover:border-black ease-in duration-300">
+                    <h1 className="text-5xl font-bold">SpellForge</h1>
+                </div>
+                <div className="w-1/3 transition hover:drop-shadow-[2px_2px_1px_rgba(0,0,0,0.45)] ease-in duration-500">
+                    <p className="text-center">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Ad tenetur ea ipsa, neque reiciendis magni
+                        explicabo delectus necessitatibus nihil veniam quaerat
+                        vero officia nisi. Pariatur inventore voluptates
+                        expedita beatae distinctio.
+                    </p>
+                </div>
             </div>
+            <div className="mb-2">
+                <p className="animate-bounce text-2xl font-bold">!^!</p>
+            </div>
+
             <div id="console" style={{ whiteSpace: "pre-line" }}>
                 <p style={{ whiteSpace: "pre-line" }}></p>
             </div>
