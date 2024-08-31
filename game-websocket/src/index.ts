38c86064e0dc8f45fa015d595b4657dd2a5f0b27
rahulbarmann <itsrahulbarman1@gmail.com>
2024-08-31 20:36:44 +0530

    refactor(game-websocket): integrate rooms in websocket for players to have their individual p2p duel session new player starts a new session and waits in the room until another player joins the duel

--- game-websocket/src/index.ts
+++ game-websocket/src/index.ts
@@ -11,35 +11,82 @@ const io = new Server(httpServer, {
         methods: ["GET", "POST"],
     },
 });
-
-let players: any = [];
-let lastTurnBy;
+//                     roomNumber:    0     1   ...
+let players: any = []; // schema: [ [{}], [{}], ... ]
+let playerCount = 0;
+let roomNumber: number;
 
 io.on("connection", (socket) => {
     console.log("New User connected with Socket ID:", socket.id);
-    lastTurnBy = socket.id;
 
-    socket.broadcast.emit("other-player", `Player ${socket.id} joined!`);
+    if (playerCount % 2 == 0) {
+        roomNumber = playerCount / 2;
+        players.push([]);
+    }
 
-    players.push({
+    console.log(players);
+
+    playerCount++;
+    socket
+        .to(`roomId-${roomNumber}`)
+        .emit("other-player", `Player ${socket.id} joined!`);
+
+    players[roomNumber].push({
         username: socket.id,
+        sid: socket.id,
         healthPoint: 100,
         cards: ["fireball", "holylight"],
+        lastPlayed: false,
     });
 
-    socket.on("use-spell", (arg) => {
-        if (lastTurnBy! === socket.id) {
+    socket.join(`roomId-${roomNumber}`);
+
+    socket.on("use-spell", async (arg) => {
+        const socketRoomNumber = Number([...socket.rooms][1].split("-")[1]);
+
+        console.log("ALL OF THE ROOMS FOR THIS SOCKET:", socket.rooms);
+        console.log(
+            "THE ROOM ID FOR THIS SOCKET:",
+            [...socket.rooms][1].split("-")[1]
+        );
+
+        const totalSockets = await io
+            .in(`roomId-${socketRoomNumber}`)
+            .fetchSockets();
+        const numberOfSockets = totalSockets.length;
+
+        console.log(
+            "NUMBER OF ACTIVE SOCKETS IN THIS ROOM:",
+            totalSockets.length
+        );
+
+        const subArr = players[socketRoomNumber].filter(
+            (e: any) => e.sid == socket.id
+        );
+
+        console.log("The SUB ARR: ", subArr, subArr[0].lastPlayed === true);
+
+        if (numberOfSockets % 2 != 0 || subArr[0].lastPlayed === true) {
             socket.emit("alert", "Wait For Your Opponent's Turn!");
         } else {
-            lastTurnBy = socket.id;
-            socket.broadcast.emit(
-                "other-player",
-                `Player ${arg.username} used the spell ${arg.spell} on you!`
-            );
+            players[socketRoomNumber].forEach((e: any) => {
+                if (e.sid == socket.id) {
+                    e.lastPlayed = true;
+                } else {
+                    e.lastPlayed = false;
+                }
+            });
+
+            socket
+                .to(`roomId-${socketRoomNumber}`)
+                .emit(
+                    "other-player",
+                    `Player ${arg.username} used the spell ${arg.spell} on you!`
+                );
             let otherPlayerHP;
             let myHP;
-            players.forEach((e: any) => {
-                if (e.username != socket.id) {
+            players[socketRoomNumber].forEach((e: any) => {
+                if (e.sid != socket.id) {
                     otherPlayerHP = e.healthPoint;
                 } else {
                     myHP = e.healthPoint;
@@ -51,8 +98,8 @@ io.on("connection", (socket) => {
             }
             test().then(() => console.log(sockets.length));
             const newStat = calculateSpell(arg.spell, myHP!, otherPlayerHP!);
-            players.forEach((e: any) => {
-                if (e.username != socket.id) {
+            players[socketRoomNumber].forEach((e: any) => {
+                if (e.sid != socket.id) {
                     e.healthPoint = newStat.otherPlayerHP;
                 } else {
                     e.healthPoint = newStat.currentPlayerHP;
@@ -64,13 +111,18 @@ io.on("connection", (socket) => {
 
             if (newStat.currentPlayerHP <= 0) {
                 socket.emit("game-over", false);
-                socket.broadcast.emit("game-over", true);
+                socket.to(`roomId-${socketRoomNumber}`).emit("game-over", true);
             } else if (newStat.otherPlayerHP <= 0) {
                 socket.emit("game-over", true);
-                socket.broadcast.emit("game-over", false);
+                socket
+                    .to(`roomId-${socketRoomNumber}`)
+                    .emit("game-over", false);
             }
 
-            io.emit("hp-revaluate", players);
+            io.to(`roomId-${socketRoomNumber}`).emit(
+                "hp-revaluate",
+                players[socketRoomNumber]
+            );
         }
     });
 });
