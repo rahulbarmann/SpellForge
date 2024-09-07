1d2bf8536ea95bfe4fbcaf8c697a2508443d3afa
rahulbarmann <itsrahulbarman1@gmail.com>
2024-09-07 05:35:32 +0530

    feat: add minting of spells as NFT's into user's wallet during onboarding

--- web/src/components/forge/SpellSelection.tsx
+++ web/src/components/forge/SpellSelection.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { SpellsArray } from "@/lib/constants";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useWriteContract, type BaseError } from "wagmi";
+import { abi, contractAddress } from "@/lib/constants";
+import { usePrivy } from "@privy-io/react-auth";
 
 function getRandomCID(arr: any, num: any) {
     const shuffled = [...arr].sort(() => 0.5 - Math.random());
@@ -10,8 +13,86 @@ function getRandomCID(arr: any, num: any) {
 
 export function SpellSelection({ setIsFirstLoad }: any) {
     const [selectedSpells, setSelectedSpells] = useState<any>([]);
+    const [isMinting, setIsMinting] = useState(false);
+    const { user, ready } = usePrivy();
+
+    const userAddress = user?.wallet?.address;
 
     const randomCIDs = useMemo(() => getRandomCID(SpellsArray, 5), []);
+
+    const {
+        isPending: isWriteLoading,
+        status: writeStatus,
+        isError: isWriteError,
+        writeContract,
+    } = useWriteContract();
+
+    useEffect(() => {
+        if (writeStatus === "pending") {
+            alert("Minting NFT");
+        } else if (
+            writeStatus === "success" &&
+            !isWriteLoading &&
+            !isWriteError
+        ) {
+            alert(
+                JSON.stringify({
+                    render: "NFT Minted",
+                    isLoading: false,
+                    autoClose: 5000,
+                    type: "success",
+                })
+            );
+            setIsMinting(false);
+        } else if (writeStatus === "error") {
+            alert(
+                JSON.stringify({
+                    render: "Error Minting NFT",
+                    isLoading: false,
+                    autoClose: 5000,
+                    type: "error",
+                })
+            );
+            setIsMinting(false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [writeStatus]);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        setIsMinting(true);
+        mintNFT([
+            randomCIDs[selectedSpells[0]],
+            randomCIDs[selectedSpells[1]],
+            randomCIDs[selectedSpells[2]],
+        ]);
+    };
+
+    const mintNFT = (URIs: string[]) => {
+        try {
+            writeContract({
+                address: contractAddress,
+                abi,
+                functionName: "mintNFT",
+                args: [userAddress, URIs[0]],
+            });
+            writeContract({
+                address: contractAddress,
+                abi,
+                functionName: "mintNFT",
+                args: [userAddress, URIs[1]],
+            });
+            writeContract({
+                address: contractAddress,
+                abi,
+                functionName: "mintNFT",
+                args: [userAddress, URIs[2]],
+            });
+        } catch (error) {
+            console.log("Error minting NFT:", error);
+            setIsMinting(false);
+        }
+    };
 
     const toggleSpell = (index: number) => {
         setSelectedSpells((prevSelected: any) => {
@@ -49,6 +130,11 @@ export function SpellSelection({ setIsFirstLoad }: any) {
 
     return (
         <div>
+            {JSON.stringify([
+                randomCIDs[selectedSpells[0]],
+                randomCIDs[selectedSpells[1]],
+                randomCIDs[selectedSpells[2]],
+            ])}
             <p>Selected Spells: {selectedSpells.length}</p>
             <div className="flex flex-col">
                 {randomCIDs.map((spellImage, index) => (
@@ -58,6 +144,24 @@ export function SpellSelection({ setIsFirstLoad }: any) {
                         index={index}
                     />
                 ))}
+            </div>
+            <div>
+                {ready ? (
+                    <div>
+                        {isMinting ? (
+                            <div className="loading-circle">Loading...</div>
+                        ) : (
+                            <button
+                                disabled={selectedSpells.length !== 3}
+                                onClick={handleSubmit}
+                            >
+                                Mint Your Spells!
+                            </button>
+                        )}
+                    </div>
+                ) : (
+                    ""
+                )}
             </div>
             <ContinueButton />
         </div>
